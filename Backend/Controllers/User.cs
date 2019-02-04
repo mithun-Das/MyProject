a71bcfbf3235e97c49a28ddaa537d59ea84f6df8
SELISE\mithun.das <mithun.das@selise.ch>
2019-02-04 18:09:19 +0600

    Project Initialized

---
+++ Backend/Controllers/User.cs
@@ -0,0 +1,15 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Backend.Controllers
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+    }
+}
